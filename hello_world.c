5ad02b28501bf431b6f6ff9de547548d2301de2e
jardinesdelareina <jardinesdelareina@mail.ru>
2023-06-24 02:00:42 +0300

    getchar, putchar

--- hello_world.c
+++ hello_world.c
@@ -1,15 +1,9 @@
-/* Вывод текстового сообщения "Hello World" */
-
-#include <stdio.h>      // включение модуля standard input output, импорт функции printf()
+#include <stdio.h>
 
 int main(void)
 {
-    char symbol, *string;
-
-    symbol = 's';           // одинарные кавычки определяют символ
-    string = "S";           // двойные кавычки определяют строку
-
-    int size_var = sizeof(symbol);
-    printf("Размер переменной: %d байт\n", size_var);
+    int value = getchar();                      // функция ввода
+    int result = putchar(value);                // функция вывода
+    printf("\nSymbol code: %d\n", result);
     return 0;
 }
