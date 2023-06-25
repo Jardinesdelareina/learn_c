65e32f5c52ac12d296f69db30eda1965dd1de20f
jardinesdelareina <jardinesdelareina@mail.ru>
2023-06-26 01:46:32 +0300

    scanf, арифметические операции

--- hello_world.c
+++ hello_world.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 
-int main(void)
-{
-    int value = getchar();                      // функция ввода
-    int result = putchar(value);                // функция вывода
-    printf("\nSymbol code: %d\n", result);
+int main(void) {
+    double num_1, num_2;
+    int input = scanf("%lf %lf", &num_1, &num_2);       // ввод двух чисел
+
+    double result = (num_1 * (num_2 - num_1)) / 2;      // арифметические действия с данными
+    double target_num = 10;
+
+    if (result > target_num) {                          // условный оператор
+        printf("Result > %f\n", target_num);
+        return 0;
+    }
+
+    printf("%.2f\n", result);                           // вывод результата
     return 0;
 }
