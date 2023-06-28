c6e4b0d76f5488b0e2ddbb33ef267af0aa405b15
jardinesdelareina <jardinesdelareina@mail.ru>
2023-06-29 02:35:21 +0300

    Синтаксис условных операторов

--- main.c
+++ main.c
@@ -3,11 +3,15 @@
 
 int main(void)
 {
-    int digit;
-    scanf("%d", &digit);
-    
-    bool range_nums = digit <= -100 || digit >= 100;
-    printf("%d", range_nums);
-
+    int num;
+    if (scanf("%d", &num) != 1) {
+        printf("Error");
+        return 0;
+    }
+    if (num > 1) {
+        if (num % 2 == 0) printf("Even");
+        else printf("Not even");
+    }
+    else printf("Input number: %d", num);
     return 0;
 }
