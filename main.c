078c4f506d7b3e7462444ca262bd72271286b610
jardinesdelareina <jardinesdelareina@mail.ru>
2023-07-10 08:18:27 +0300

    Генерация псевдослучайных чисел

--- main.c
+++ main.c
@@ -1,26 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main(void)
+int main(void) 
 {
-    char symbol;
-    if (scanf("%c", &symbol) != 1) {
-        printf("Error");
-    }
+    // устанавливает "зерно" для генератора случайных чисел, используя текущее время в качестве параметра,
+    // это служит для того, чтобы каждый раз при запуске программы генерировались разные случайные числа 
+    srand(time(NULL));
 
-    // break - выход из цикла switch case, 
-    // return 0 - выход из программы
-    switch(symbol) {
-        case 'a':
-        case 'A':
-            printf("Symbol A\n");
-            break;
-        case 'b':
-        case 'B':
-            printf("Symbol B\n");
-            break;
-        default:
-            printf("Any symbol\n");
-    }
+    int random_num_1 = rand();
+    int random_num_2 = rand();
+    int sum_random = random_num_1 + random_num_2;
+
+    printf("%d, %d, %d", random_num_1, random_num_2, sum_random);
 
     return 0;
 }
