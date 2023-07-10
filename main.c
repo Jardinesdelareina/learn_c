0c83b78aa226bf0d21e1352cb89aaf486752171e
jardinesdelareina <jardinesdelareina@mail.ru>
2023-07-11 00:42:46 +0300

    #include

--- main.c
+++ main.c
@@ -1,27 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-/* Для переопределения макроса нельзя написать новое значение в следующей строке, нужно отменить определение 
-с помощью #undef и задать новое значение */
-
-#define RANDOM_NUM_FROM_TIME time(NULL)         // определение макроса
-
-#undef RANDOM_NUM_FROM_TIME                     // отмена определения макроса
-
-#define RANDOM_NUM_FROM_TIME time(NULL) % 10    // повторное определение макроса
+#include "printf.h"
 
 int main(void) 
 {
-    // устанавливает "зерно" для генератора случайных чисел, используя текущее время в качестве параметра,
-    // это служит для того, чтобы каждый раз при запуске программы генерировались разные случайные числа 
-    srand(RANDOM_NUM_FROM_TIME);
-
-    int random_num_1 = rand();
-    int random_num_2 = rand();
-    int sum_random = random_num_1 + random_num_2;
-
-    printf("%d, %d, %d", random_num_1, random_num_2, sum_random);
-
+    printf("Hello\n")
     return 0;
 }
