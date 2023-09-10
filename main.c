c32f63d6f7e9ba3876b9199308f19af2f3974471
jardinesdelareina <jardinesdelareina@mail.ru>
2023-09-10 03:06:01 +0300

    // update readme

--- main.c
+++ main.c
@@ -3,16 +3,14 @@
 int main(void)
 {
     int num = 31;
-    int *pointer_1, *pointer_2;
+    int *pointer_1;
+    char *pointer_2;
 
-    pointer_1 = &num;
-    pointer_2 = pointer_1;
+    *pointer_1 = &num;
+    *pointer_2 = (char *)pointer_1;
 
-    printf("*pointer_2 = %d, num = %d\n", *pointer_2, num);
-
-    *pointer_1 = 32;
-
-    printf("*pointer_2 = %d, num = %d\n", *pointer_2, num);
+    *pointer_2 = 32;
+    printf("*pointer_1 = %d, num = %d\n", *pointer_1, num);
 
     return 0;
 }
