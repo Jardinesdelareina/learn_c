aa80b8d5755e0688d8f01a6251baab50b4962615
jardinesdelareina <jardinesdelareina@mail.ru>
2023-09-27 21:22:13 +0300

    Двумерные массивы

--- main.c
+++ main.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
 
+#define num_1 3
+#define num_2 2
+
 int main(void)
 {
-    int num = 31;
-    int *pointer_1;
-    char *pointer_2;
+    int field[num_1][num_2] = {1, 2, 3, 4, 5, 6};
 
-    *pointer_1 = &num;
-    *pointer_2 = (char *)pointer_1;
+    for(int h = 0; h < num_1; ++h) {
+        for(int v = 0; v < num_2; ++v)
+            printf("%d", field[h][v]);
+        printf("\n");
+    }
 
-    *pointer_2 = 32;
-    printf("*pointer_1 = %d, num = %d\n", *pointer_1, num);
+    size_t bytes_field = sizeof(field);
+    printf("Size array: %d", bytes_field);
 
     return 0;
 }
+
+/* 
+Вывод массива будет выглядеть так:
+1   2
+3   4
+5   6
+*/
