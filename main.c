6054f7886f044d8459d3fe0479668ff4b5f3ddab
jardinesdelareina <jardinesdelareina@mail.ru>
2023-09-27 21:53:15 +0300

    Экранирование и спецсимволы

--- main.c
+++ main.c
@@ -1,27 +1,9 @@
 #include <stdio.h>
 
-#define num_1 3
-#define num_2 2
-
-int main(void)
+int main(void) 
 {
-    int field[num_1][num_2] = {1, 2, 3, 4, 5, 6};
-
-    for(int h = 0; h < num_1; ++h) {
-        for(int v = 0; v < num_2; ++v)
-            printf("%d", field[h][v]);
-        printf("\n");
-    }
-
-    size_t bytes_field = sizeof(field);
-    printf("Size array: %d", bytes_field);
+    char string[] = "Programming in \"С\" language";
+    puts(string);
 
     return 0;
 }
-
-/* 
-Вывод массива будет выглядеть так:
-1   2
-3   4
-5   6
-*/
