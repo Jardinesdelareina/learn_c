5114380c40f1875d821fe49ce4f2e5ed64fab3df
jardinesdelareina <jardinesdelareina@mail.ru>
2023-10-01 22:02:06 +0300

    Указатели как параметры

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
-#include "src/math_sqrt.c"
+
+void get_pointers(int* one, int* two)
+{
+    int three = *one;
+    *one = *two;
+    *two = three;
+}
 
 int main(void)
 {
-    printf("%f", sqrt(81));
+    int num_1 = 1, num_2 = 2;
+    get_pointers(&num_1, & num_2);
+    printf("num_1 = %d\nnum_2 = %d", num_1, num_2);
     return 0;
 }
