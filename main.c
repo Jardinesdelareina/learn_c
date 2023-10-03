583a27723fc08e93e26772ef990b94700a6b75b7
jardinesdelareina <jardinesdelareina@mail.ru>
2023-10-04 00:30:23 +0300

    Указатели как параметры функции

--- main.c
+++ main.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 
+int sum_array(const int*array, int length)
+{
+    /* Перебирает элементы области памяти и вычисляет их сумму */
+    int result = 0;
+    for(int el = 0; el < length; ++el)
+        result += array[el];
+    return result;
+}
+
 int main(void)
 {
-    const char array[] = {5, 43, 3, 1, -5, 14};     // const перет инициализацией массива делает 
-                                                    // массив набором констант
-    
-    const char *ptr_1 = array;                      // Инициализация указателя и запрет вносить
-                                                    // изменения в значения, хранящиеся в данной
-                                                    // ячейке памяти
-    
-    char *prt_2 = array;                            // Инициализация указателя, который может и читать 
-                                                    // и записывать информацию в ячейку памяти
+    int any_array[] = {4, 3, 55, -6};
 
+    // Расчет количества элементов в области памяти
+    int count_element = sizeof(any_array) / sizeof(*any_array);
+    int value = sum_array(any_array, count_element);
+
+    printf("Sum array: %d", value);
     return 0;
 }
