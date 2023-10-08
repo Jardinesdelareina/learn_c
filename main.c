1a8eedfd5d9d6787aff446a33c4b109d0fd33816
jardinesdelareina <jardinesdelareina@mail.ru>
2023-10-08 23:13:50 +0300

    Указатели на структуры

--- main.c
+++ main.c
@@ -1,39 +1,20 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
-enum {name_len=50, b_len=20};
-
-struct name_person {
-    char first_name[name_len];
-    char last_name[name_len];
-};
-
-struct person {
-    struct name_person name;
-    char sex;
-    unsigned short old;
-    char date_of_birdth[b_len];
+struct figure {
+    double x;
+    double y;
 };
 
 int main(void)
 {
-    struct person blank = {
-        {"Andrey", "Kostyuk"},
-        'M',
-        31,
-        "01.01.2023"
-    };
+    struct figure *ptr_rectangle = malloc(sizeof(struct figure));
 
-    blank.old = 32;
-    strcpy(blank.date_of_birdth, "02.01.2023");
+    ptr_rectangle->x = 30;
+    ptr_rectangle->y = 40;
 
-    printf(
-        "%s %s\n%c\n%d\n%s", 
-        blank.name.first_name, 
-        blank.name.last_name, 
-        blank.sex, 
-        blank.old, 
-        blank.date_of_birdth
-    );
+    printf("x: %.3f\ny: %.3f", ptr_rectangle->x, ptr_rectangle->y);
+
+    free(ptr_rectangle);
     return 0;
 }
