6043b5c21755b648f80807ca5fb625f4d1975020
jardinesdelareina <jardinesdelareina@mail.ru>
2023-10-08 22:00:38 +0300

    Структуры

--- main.c
+++ main.c
@@ -1,17 +1,39 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
-typedef enum 
-{
-    binance=3000,
-    bybit=100,
-    huobi=0
-} VARIABLE;
+enum {name_len=50, b_len=20};
+
+struct name_person {
+    char first_name[name_len];
+    char last_name[name_len];
+};
+
+struct person {
+    struct name_person name;
+    char sex;
+    unsigned short old;
+    char date_of_birdth[b_len];
+};
 
 int main(void)
 {
-    VARIABLE balance_bybit = bybit;
-    printf("Binance: %d\nBybit: %d\nHuobi: %d\n", binance, bybit, huobi);
+    struct person blank = {
+        {"Andrey", "Kostyuk"},
+        'M',
+        31,
+        "01.01.2023"
+    };
 
+    blank.old = 32;
+    strcpy(blank.date_of_birdth, "02.01.2023");
+
+    printf(
+        "%s %s\n%c\n%d\n%s", 
+        blank.name.first_name, 
+        blank.name.last_name, 
+        blank.sex, 
+        blank.old, 
+        blank.date_of_birdth
+    );
     return 0;
 }
