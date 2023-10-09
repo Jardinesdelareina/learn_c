185e151a060a8e56b5f36c958a4436454ce3874a
jardinesdelareina <jardinesdelareina@mail.ru>
2023-10-09 20:48:26 +0300

    Работа с файлами

--- main.c
+++ main.c
@@ -1,46 +1,24 @@
 #include <stdio.h>
 
-typedef enum {
-    union_none,
-    union_char,
-    union_int,
-    union_double
-} TYPE_VAR;
-
-union var_item {
-    char var_char;
-    int var_int;
-    double var_double;
-};
-
-typedef struct {
-    union var_item var;
-    TYPE_VAR type;
-} VAR;
-
-void (show_var(VAR v))
-{
-    switch(v.type) {
-        case union_char:
-            printf("var_char: %c\n", v.var.var_char);
-            break;
-        case union_int:
-            printf("var_int: %d\n", v.var.var_int);
-            break;
-        case union_double:
-            printf("var_double: %.2f\n", v.var.var_double);
-            break;
-        case union_none:
-            puts("Undefined type");
-    }
-}
+#define MAX_LENGTH 100
 
 int main(void)
 {
-    VAR var;
-    var.var.var_char = 'C';
-    var.type = union_char;
+    char string[] = "Learning C language";
 
-    show_var(var);
+    // Запись данных в файл
+    FILE* file_write = fopen("new_file.txt", "w");
+    if(file_write) {
+        fputs(string, file_write);
+        fclose(file_write);
+    }
+
+    // Чтение данных из файла
+    FILE* file_read = fopen("new_file.txt", "r");
+    if(file_read) {
+        fgetc(file_read);
+        fclose(file_read);
+    }
+
     return 0;
 }
