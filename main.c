f83b4489ed277466315196c270e00e347511a12b
JustSomeBeans <102830580+JustSomeBeans@users.noreply.github.com>
2023-10-17 11:21:41 -0700

    Update main.c
    rewrite for clarity

--- main.c
+++ main.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 #include <assert.h>
 
-float fahr(int cel)
-{
-    return cel * 1.8 + 32;
+float fahrenheit(int celsius) {
+    return celsius * 1.8 + 32;
 }
 
+int main() {
+    int celsius;
+    float result;
 
-int main()
-{
-    int cel;
-    float f;
+    assert(32 == fahrenheit(0));
+    assert(122 == fahrenheit(50));
+    assert(-40 == fahrenheit(-40));
+    assert(33.8 == fahrenheit(1));
 
-    assert(32 == fahr(0));
-    assert(122 == fahr(50));
-    assert(-40 == fahr(-40));
-    assert(33.8 == fahr(1));
+    printf("Enter temperature in Celsius: ");
+    scanf("%d", &celsius);
 
-    scanf("%d", &cel);
-    printf("%.2f", fahr(cel));
+    result = fahrenheit(celsius);
+    printf("Temperature in Fahrenheit: %.2f\n", result);
+
     return 0;
 }
