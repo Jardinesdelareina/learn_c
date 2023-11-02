5612f6228223342e0089c33d79d62c5e0725f218
Jardinesdelareina <jardinesdelareina@mail.ru>
2023-11-02 19:43:25 +0300

    strlen() <string.h>

--- main.c
+++ main.c
@@ -1,30 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-    int wallet = 0;
-    int gold = 10;
-    int hp_g = 100;
-    int kick = 20;
-    char attack;
-
-    printf("Вы атакованы гоблином! Гоблин: %d\n", hp_g);
-    attack = getchar();
-    while(attack != 'r') {
-        if(attack == 'a') {
-            puts("Вы атакуете гоблина!");
-            hp_g -= kick;
-            if(hp_g != 0) printf("Гоблин: %d\n", hp_g);
-        }
-        if(hp_g <= 0) {
-            puts("Вы убили гоблина!");
-            printf("Вы получили %d золота\n", gold);
-            printf("У вас в кошельке %d золота\n", wallet+=gold);
-            break;
-        }
-        getchar();
-        attack = getchar();
-    }
-
+    char text[] = "bfiuv";
+    int len = strlen(text);
+    printf("%d\n", len);
     return 0;
 }
