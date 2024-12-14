92b6851c0d643144590d74f1c3738a47540425f5
KOSTYUK51847_EXT <kostyuk.a@konsom.ru>
2024-12-14 23:09:24 +0300

    sizeof

--- main.c
+++ main.c
@@ -2,10 +2,9 @@
 
 int main(void)
 {
-    int x = 6678;
-    char* ptr_x = (char *)&x;
-
-    for(int i = 0; i < sizeof(x); ++i)
-        printf("| %d |", *ptr_x++);
+    printf("long double: %d", sizeof(long double));
     return 0;
 }
+
+
+// gcc main.c -o main -Wall -Wextra -std=c99 -pedantic
